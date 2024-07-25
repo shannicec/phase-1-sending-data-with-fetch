be4316a039f90b29ec5bcd058bc81f09db4076ea
shannicec <shanice.cheptoo@student.moringaschool.com>
2024-07-25 22:44:13 +0300

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email) {
+    const formData = {
+      name: name,
+      email: email
+    };
+  
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(response => response.json())
+      .then(object => {
+        document.body.innerHTML = `<p>New ID: ${object.id}</p>`;
+        return object;
+      })
+      .catch(error => {
+        document.body.innerHTML = `<p>Error: ${error.message}</p>`;
+      });
+  }
+  
